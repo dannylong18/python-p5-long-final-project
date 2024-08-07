2b0caa356c8417901dde3b1a75c4271590c0252f
Daniel Long <danielwlong618@gmail.com>
2024-08-07 17:57:33 -0500

    models initialized

---
+++ server/migrations/versions/9fdf58d41bc1_create_db.py
@@ -0,0 +1,24 @@
+"""create db
+
+Revision ID: 9fdf58d41bc1
+Revises: 
+Create Date: 2024-08-01 17:10:50.426288
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9fdf58d41bc1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -4,3 +4,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    pass
+class Doctor(db.Model, SerializerMixin):
+    pass
+class Review(db.Model, SerializerMixin):
+    pass
