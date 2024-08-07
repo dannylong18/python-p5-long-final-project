186b8e1eeb4557cbe468e2de4a2d6b751308c85e
Daniel Long <danielwlong618@gmail.com>
2024-08-07 18:59:35 -0500

    attributes added

--- server/models.py
+++ server/models.py
@@ -5,8 +5,26 @@ from config import db
 
 # Models go here!
 class User(db.Model, SerializerMixin):
-    pass
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    
 class Doctor(db.Model, SerializerMixin):
-    pass
+    __tablename__ = 'doctors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    specialty = db.Column(db.String)
+    bio = db.Column(db.String)
+
 class Review(db.Model, SerializerMixin):
-    pass
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.String)
+    time_created = db.Column(db.DateTime, server_default=db.func.now())
+    time_updated = db.Column(db.DateTime, onupdate=db.func.now())
